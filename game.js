e4afbee94e7e9b854c5ba55ddffa73bc34f333bc
Anonymous <daniel@causebrook.co.uk>
2015-12-17 11:10:36 +0000

    Fixed water height issue, and prevented player from leaving map boundaries.

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ var mapScale = 3;//scale of features will be 2^mapscale
 var mapVariance = 0.4;
 //var mapBiome;// 0-water 1-grassland, 2-forest, 3-desert, 4-rock, 5-beach, 6-marsh
 var maps;
+var canvasScale = 5;
 var plyPos;
 var godMode = true;
 
@@ -39,44 +40,65 @@ function submitText(text) {
     NL();
   }else if(spltTxt[0] === "walk"){
   	if(spltTxt[1]==="north"||spltTxt[1]==="n"){
-  		var arg2 = spltTxt[2];
-  		if(!isNaN(+arg2)){
-  			arg2=Math.abs(Math.round(arg2));
-  		    plyPos[0]-=arg2;
-            writeTxt("You walk north "+arg2+"km.");
+  		var dist = spltTxt[2];
+  		if(!isNaN(+dist)){
+  			dist=Math.abs(Math.round(dist));
+            writeTxt("You walk north "+dist+"km");
   		} else {
-  			plyPos[0]--;
-            writeTxt("You walk north.");
+  			dist=1;
+            writeTxt("You walk north");
+  		}
+  		if(plyPos[0]-dist>=0){
+  		    plyPos[0]-=dist;
+  		    writeTxt(".");
+  		} else {
+  			writeTxt(", but can't make it. There seems to be some sort of barrier blocking your way.");
   		}
   	} else if(spltTxt[1]==="south"||spltTxt[1]==="s"){
-  		var arg2 = spltTxt[2];
-  		if(!isNaN(+arg2)){
-  			arg2=Math.abs(Math.round(arg2));
-  		    plyPos[0]+=arg2;
-            writeTxt("You walk south "+arg2+"km.");
+  		var dist = spltTxt[2];
+  		if(!isNaN(+dist)){
+  			dist=Math.abs(Math.round(dist));
+            writeTxt("You walk south "+dist+"km");
   		} else {
-  			plyPos[0]++;
-            writeTxt("You walk south.");
+  			dist=1;
+            writeTxt("You walk south");
+  		}
+  		if(plyPos[0]+dist<maps[0][0].length){
+  		    plyPos[0]+=dist;
+  		    writeTxt(".");
+  		} else {
+  			writeTxt(", but can't make it. There seems to be some sort of barrier blocking your way.");
   		}
   	} else if(spltTxt[1]==="east"||spltTxt[1]==="e"){
-  		var arg2 = spltTxt[2];
-  		if(!isNaN(+arg2)){
-  			arg2=Math.abs(Math.round(arg2));
-  		    plyPos[1]+=arg2;
-            writeTxt("You walk east "+arg2+"km.");
+  		var dist = spltTxt[2];
+  		if(!isNaN(+dist)){
+  			dist=Math.abs(Math.round(dist));
+            writeTxt("You walk east "+dist+"km");
   		} else {
-  			plyPos[1]++;
-        	writeTxt("You walk east.");
+  			dist=1;
+        	writeTxt("You walk east");
+  		}
+  		if(plyPos[1]+dist<maps[0].length){
+  		    plyPos[1]+=dist;
+  		    writeTxt(".");
+  		} else {
+  			writeTxt(", but can't make it. There seems to be some sort of barrier blocking your way.");
   		}
   	} else if(spltTxt[1]===" west"||spltTxt[1]==="w"){
-  		var arg2 = spltTxt[2];
-  		if(!isNaN(+arg2)){
-  			arg2=Math.abs(Math.round(arg2));
-  		    plyPos[1]-=arg2;
-            writeTxt("You walk west "+arg2+"km.");
+  		var dist = spltTxt[2];
+  		if(!isNaN(+dist)){
+  			dist=Math.abs(Math.round(dist));
+            writeTxt("You walk west "+dist+"km");
   		} else {
+  			dist=1;
   			plyPos[1]--;
-            writeTxt("You walk west.");
+            writeTxt("You walk west");
+  		}
+  		if(plyPos[1]-dist>=0){
+  		    plyPos[1]-=dist;
+  		    writeTxt(".");
+  		} else {
+  			writeTxt(", but can't make it. There seems to be some sort of barrier blocking your way.");
   		}
   	} else {
        writeTxt("You walk forwards and derpily trip over.");
@@ -84,7 +106,7 @@ function submitText(text) {
     NL();
   }else if(spltTxt[0] === "scout"){
     writeTxtSty("You look around you, and you see:");
-    printVisMap(plyPos,15,checkVisibility(plyPos,15,maps[0]),maps[0],maps[2],true);
+    printVisMap(plyPos,15,checkVisibility(plyPos,15,maps[0],maps[2]),maps[0],maps[2],true);
     NL();
     NL();
   }else{
@@ -111,8 +133,8 @@ function init() {
   plyPos=[Math.round(mapHeight.length / 2),Math.round(mapHeight[0].length / 2)];
   //plyPos=[50,50];
   //Take a break! http://xkcd.com/kite/kite_trick.jpg
-  printVisMap(plyPos,15,checkVisibility(plyPos,15,mapHeight),mapHeight,mapBiome,false);
-  printVisMap(plyPos,15,checkVisibility(plyPos,15,mapHeight),mapHeight,mapBiome,true);
+  printVisMap(plyPos,15,checkVisibility(plyPos,15,mapHeight,mapBiome),mapHeight,mapBiome,false);
+  printVisMap(plyPos,15,checkVisibility(plyPos,15,mapHeight,mapBiome),mapHeight,mapBiome,true);
   NL();
 }
 
@@ -122,8 +144,12 @@ function printVisMap(pos,vis,vM,hM,bM,mask) {
 	for(i=0;i<2*vis-1;i++){
 		pM[i] = new Array(vis*2-1);
 		for(j=0;j<2*vis-1;j++){
-			if(vM[i+pos[0]-vis+1][j+pos[1]-vis+1]===1||!mask){
-				pM[i][j] = [hM[i+pos[0]-vis+1][j+pos[1]-vis+1],bM[i+pos[0]-vis+1][j+pos[1]-vis+1]];
+			if(i+pos[0]-vis+1>=0&&j+pos[1]-vis+1>=0&&i+pos[0]-vis+1<vM.length&&j+pos[1]-vis+1<vM[0].length){
+				if(vM[i+pos[0]-vis+1][j+pos[1]-vis+1]===1||!mask){
+					pM[i][j] = [hM[i+pos[0]-vis+1][j+pos[1]-vis+1],bM[i+pos[0]-vis+1][j+pos[1]-vis+1]];
+				} else {
+					pM[i][j] = [-1,-1];
+				}
 			} else {
 				pM[i][j] = [-1,-1];
 			}
@@ -132,8 +158,8 @@ function printVisMap(pos,vis,vM,hM,bM,mask) {
 	
   NL();
   var e = document.createElement('canvas');
-  e.width = 4*pM.length;
-  e.height = 4*pM[0].length;
+  e.width = canvasScale*pM.length;
+  e.height = canvasScale*pM[0].length;
   var ctx = e.getContext("2d");
   for(i=0;i<pM.length;i++) {
       for(j=0;j<pM[i].length;j++) {
@@ -154,14 +180,14 @@ function printVisMap(pos,vis,vM,hM,bM,mask) {
 	      } else if (pM[i][j][1]===6) {
 	        ctx.fillStyle = "hsl(180, 60%, "+(50+pM[i][j][0]*20)+"%)";
 	      }
-	      ctx.fillRect(j*4,i*4,4,4);
+	      ctx.fillRect(j*canvasScale,i*canvasScale,canvasScale,canvasScale);
 	      if(i===vis&&j===vis){
 	      	ctx.fillStyle = "#FF0000";
-	      	ctx.moveTo(j*4,i*4);
-			ctx.lineTo(j*4+3,i*4+3);
+	      	ctx.moveTo(j*canvasScale,i*canvasScale);
+			ctx.lineTo((j+1)*canvasScale-1,(i+1)*canvasScale-1);
 			ctx.stroke();
-	      	ctx.moveTo(j*4+3,i*4);
-			ctx.lineTo(j*4,i*4+3);
+	      	ctx.moveTo((j+1)*canvasScale-1,i*canvasScale);
+			ctx.lineTo(j*canvasScale,(i+1)*canvasScale-1);
 			ctx.stroke();
 	      }
       }
@@ -169,10 +195,15 @@ function printVisMap(pos,vis,vM,hM,bM,mask) {
   document.body.lastElementChild.appendChild(e);
 }
 
-function checkVisibility(pos,vis,hM) {
+function checkVisibility(pos,vis,hM,bM) {
 	var mapVis = JSON.parse(JSON.stringify(hM));
 	var mapGrad = JSON.parse(JSON.stringify(hM));
-	var posH = hM[pos[0]][pos[1]]+0.15;
+	var posH;
+	if(bM[pos[0]][pos[1]]===0){
+		posH = -0.85;
+	} else {
+		posH = hM[pos[0]][pos[1]]+0.15;
+	}
 	var i,j;
 	for(i=0;i<mapVis.length;i++){
 		for(j=0;j<mapVis[i].length;j++){
@@ -189,59 +220,70 @@ function checkVisibility(pos,vis,hM) {
 	}
 	for(i=pos[0]-vis;i<=pos[0]+vis;i++){
 	    for(j=pos[1]-vis;j<=pos[1]+vis;j++){
-		    if(mapVis[i][j]===1){
-                mapGrad[i][j]=(hM[i][j]-posH)/Math.sqrt(Math.pow(i-pos[0],2)+Math.pow(j-pos[1],2));
-            }
+	    	if(i>=0&&j>=0&&i<mapVis.length&&j<mapVis[0].length){
+			    if(mapVis[i][j]===1){
+			    	if(bM[i][j]===0){
+	                	mapGrad[i][j]=(-1-posH)/Math.sqrt(Math.pow(i-pos[0],2)+Math.pow(j-pos[1],2));
+	                } else {
+		                mapGrad[i][j]=(hM[i][j]-posH)/Math.sqrt(Math.pow(i-pos[0],2)+Math.pow(j-pos[1],2));
+	                }
+
+	            }
+	        }
+
         }
 	}
 	for(i=pos[0]-vis;i<=pos[0]+vis;i++){
 	    for(j=pos[1]-vis;j<=pos[1]+vis;j++){
-	    	if(mapVis[i][j]===1){
-	    		var graddx = (j-pos[1]) / (i-pos[0]); //Essentially gradient from (i,j) to pos
-	    		var graddy = (i-pos[0]) / (j-pos[1]);
-	    		var a,b;
-	    		var visible = true;
-	    		if(i<pos[0]){
-		    		for(a=0;a<pos[0]-1-i;a++){
-		    			var y = Math.floor((a+0.5)*graddx+j+0.5);
-		    			if(mapGrad[a+i][y]>mapGrad[i][j]||mapGrad[a+i+1][y]>mapGrad[i][j]){
-		    				visible=false;
-		    			}
-		    			/*if(i+8===pos[0]&&j-10===pos[1]){
-	    			        console.log("i:"+i+"; j:"+j+"; graddx:"+graddx+"; graddy:"+graddy+"; posHeight:"+posH+"; posgradient:"+mapGrad[i][j]+"; x:"+(a+i)+"; y:"+y+"; height:"+hM[a+i][y]+"; gradient:"+mapGrad[a+i][y]+"; visible:"+visible);
-	    			        console.log("i:"+i+"; j:"+j+"; graddx:"+graddx+"; graddy:"+graddy+"; posHeight:"+posH+"; posgradient:"+mapGrad[i][j]+"; x:"+(a+i+1)+"; y:"+y+"; height:"+hM[a+i+1][y]+"; gradient:"+mapGrad[a+i+1][y]+"; visible:"+visible);
-	    		        }*/
-		    		}
-	    		} else {
-			    	for(a=0;a<i-1-pos[0];a++){
-			    		var y = Math.floor((a+0.5)*graddx+pos[1]+0.5);
-			    		if(mapGrad[a+pos[0]][y]>mapGrad[i][j]||mapGrad[a+pos[0]+1][y]>mapGrad[i][j]){
-			    			visible=false;
+	    	if(i>=0&&j>=0&&i<mapVis.length&&j<mapVis[0].length){
+		    	if(mapVis[i][j]===1){
+		    		var graddx = (j-pos[1]) / (i-pos[0]); //Essentially gradient from (i,j) to pos
+		    		var graddy = (i-pos[0]) / (j-pos[1]);
+		    		var a,b;
+		    		var visible = true;
+		    		if(i<pos[0]){
+			    		for(a=0;a<pos[0]-1-i;a++){
+			    			var y = Math.floor((a+0.5)*graddx+j+0.5);
+			    			if(mapGrad[a+i][y]>mapGrad[i][j]||mapGrad[a+i+1][y]>mapGrad[i][j]){
+			    				visible=false;
+			    			}
+			    			/*if(i+8===pos[0]&&j-10===pos[1]){
+		    			        console.log("i:"+i+"; j:"+j+"; graddx:"+graddx+"; graddy:"+graddy+"; posHeight:"+posH+"; posgradient:"+mapGrad[i][j]+"; x:"+(a+i)+"; y:"+y+"; height:"+hM[a+i][y]+"; gradient:"+mapGrad[a+i][y]+"; visible:"+visible);
+		    			        console.log("i:"+i+"; j:"+j+"; graddx:"+graddx+"; graddy:"+graddy+"; posHeight:"+posH+"; posgradient:"+mapGrad[i][j]+"; x:"+(a+i+1)+"; y:"+y+"; height:"+hM[a+i+1][y]+"; gradient:"+mapGrad[a+i+1][y]+"; visible:"+visible);
+		    		        }*/
 			    		}
-			    	}
-	    		}
-	    		if(j<pos[1]){
-		    		for(a=0;a<pos[1]-1-j;a++){
-		    			var x = Math.floor((a+0.5)*graddy+i+0.5);
-		    			if(mapGrad[x][a+j]>mapGrad[i][j]||mapGrad[x][a+j+1]>mapGrad[i][j]){
-		    				visible=false;
-		    			}
+		    		} else {
+				    	for(a=0;a<i-1-pos[0];a++){
+				    		var y = Math.floor((a+0.5)*graddx+pos[1]+0.5);
+				    		if(mapGrad[a+pos[0]][y]>mapGrad[i][j]||mapGrad[a+pos[0]+1][y]>mapGrad[i][j]){
+				    			visible=false;
+				    		}
+				    	}
 		    		}
-	    		} else {
-			    	for(a=0;a<j-1-pos[1];a++){
-			    		var x = Math.floor((a+0.5)*graddy+pos[0]+0.5);
-			    		if(mapGrad[x][a+pos[1]]>mapGrad[i][j]||mapGrad[x][a+pos[1]+1]>mapGrad[i][j]){
-			    			visible=false;
-		    			}
-			    	}
-	    		}
-	    		if(!visible){
-	    			mapVis[i][j] = 0;
-	    		} else {
-	    			mapVis[i][j] = 1;
-	    		}
-
+		    		if(j<pos[1]){
+			    		for(a=0;a<pos[1]-1-j;a++){
+			    			var x = Math.floor((a+0.5)*graddy+i+0.5);
+			    			if(mapGrad[x][a+j]>mapGrad[i][j]||mapGrad[x][a+j+1]>mapGrad[i][j]){
+			    				visible=false;
+			    			}
+			    		}
+		    		} else {
+				    	for(a=0;a<j-1-pos[1];a++){
+				    		var x = Math.floor((a+0.5)*graddy+pos[0]+0.5);
+				    		if(mapGrad[x][a+pos[1]]>mapGrad[i][j]||mapGrad[x][a+pos[1]+1]>mapGrad[i][j]){
+				    			visible=false;
+			    			}
+				    	}
+		    		}
+		    		if(!visible){
+		    			mapVis[i][j] = 0;
+		    		} else {
+		    			mapVis[i][j] = 1;
+		    		}
+	
+		    	}
 	    	}
+
     	}
 	}
 	return mapVis;
