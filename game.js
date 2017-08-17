b6469c12355e879511f7f240b361b2cd7fba91f3
TauNeutrin0 <tauneutrin00@gmail.com>
2017-08-17 00:59:12 +0000

    Move map-related code and improve maintainability
    - Move map related code to new file mapTools.js.
    - Store all biome data in single variable and use that.
    - Improve and standardise map manipulation, especially when converted to colors.

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
-/* eslint -env browser */
+/*eslint-disable no-param-reassign */
 var mapSize = [360, 360];
 var mapScale = 3; // scale of features will be 2^mapscale
 var mapVariance = 0.4;
-// var mapBiome; // 0 - water 1 - grassland, 2 - forest, 3 - desert, 4 - rock, 5 - beach, 6 - marsh
+
 var maps;
 var knownMaps;
 var canvasScale = 6;
@@ -24,20 +24,20 @@ function writeTxt(text) {
     document.body.lastElementChild.appendChild(text);
   }
 }
+
 function writeTxtNL(text) {
   writeTxt(text);
   NL();
 }
 
 function NL() {
-  // document.body.appendChild(document.createElement('br'));
   var line = document.createElement('div');
   line.className = 'line';
   document.body.appendChild(line);
 }
 
 function walk(dy, dx, descr, dist) {
-  if (!isNaN(+dist)) {
+  if (!isNaN(Number(dist))) {
     dist = Math.abs(Math.round(dist));
     writeTxt("You walk " + descr + ", " + dist + "km");
   } else {
@@ -64,6 +64,7 @@ function submitText(text) {
   window.scrollTo(0,document.body.scrollHeight);
   input.value = "";
 }
+
 function handleText(text) {
   text = text.toLowerCase();
   var spltTxt = text.split(" ");
@@ -95,18 +96,18 @@ function handleText(text) {
     }
   } else if (spltTxt[0] === "scout" || spltTxt[0] === "s") {
     writeTxtSty("You look around you, and you see:");
-    var vismap = checkVisibility(plyPos, 15, maps[0], maps[2]);
+    var vismap = checkMapVisibility(plyPos, 15, maps[0], maps[2]);
     copyMaps(maps, knownMaps, vismap);
-    printVisMap(plyPos, 15, vismap, maps[0], maps[2], true);
+    writeVisMap(plyPos, 15, vismap, maps[0], maps[2], true);
     NL();
     NL();
   } else if (spltTxt[0] === "map" || spltTxt[0] === "m") {
     if (godMode) {
       writeTxtSty("You use the awesome power of the devs, and the entire world is revealed:");
-      writeMapBiome(maps[2], maps[0], plyPos);
+      writeColorMap(generateColorMap(maps[2], maps[0]), plyPos);
     } else {
       writeTxtSty("You try to remember the maps you've already made...");
-      writeMapBiome(knownMaps[2], knownMaps[0], plyPos);
+      writeColorMap(generateColorMap(knownMaps[2], knownMaps[0]), plyPos);
     }
     NL();
   } else if (spltTxt[0] === "help" || spltTxt[0] === "?") {
@@ -130,9 +131,9 @@ function init() {
   NL();
   NL();
   writeTxtNL("You wake up lying on the floor. Where are you? I guess you'd better find out.");
-  var mapHeight = initMap(mapSize, mapScale, mapVariance, true);
-  var mapHumidity = initMap(mapSize, mapScale, mapVariance, false);
-  var mapBiome = genMapBiome(mapHeight, mapHumidity);
+  var mapHeight = initTerrainMap(mapSize, mapScale, mapVariance, true);
+  var mapHumidity = initTerrainMap(mapSize, mapScale, mapVariance, false);
+  var mapBiome = generateBiomeMap(mapHeight, mapHumidity);
   maps = new Array(3);
   maps[0] = mapHeight;
   maps[1] = mapHumidity;
@@ -148,7 +149,7 @@ function init() {
   writeTxtNL("You look around...");
   // Take a break! http://xkcd.com/kite/kite_trick.jpg
   //printVisMap(plyPos, 15, checkVisibility(plyPos, 15, mapHeight, mapBiome), mapHeight, mapBiome, false);
-  printVisMap(plyPos, 15, checkVisibility(plyPos, 15, mapHeight, mapBiome), mapHeight, mapBiome, true);
+  writeVisMap(plyPos, 15, checkMapVisibility(plyPos, 15, mapHeight, mapBiome), mapHeight, mapBiome, true);
   NL();
   NL();
   var input = document.getElementById("input");
@@ -156,193 +157,41 @@ function init() {
   input.focus();
 }
 
-function printVisMap(pos, vis, vM, hM, bM, mask) {
-  var pM = new Array(vis * 2 - 1);
+function writeVisMap(pos, vis, vM, hM, bM, mask) {
+  // Calculate offset on tiles and apply visibility mask and range of sight
+  var visHMap = new Array(vis * 2 - 1);
+  var visBMap = new Array(vis * 2 - 1);
   var i, j;
   for (i = 0; i < 2 * vis - 1; i++) {
-    pM[i] = new Array(vis * 2 - 1);
+    visHMap[i] = new Array(vis * 2 - 1);
+    visBMap[i] = new Array(vis * 2 - 1);
     for (j = 0; j < 2 * vis - 1; j++) {
-      if (i + pos[0] - vis + 1 >= 0 && j + pos[1] - vis + 1 >= 0 && i + pos[0] - vis + 1 < vM.length && j + pos[1] - vis + 1 < vM[0].length) {
         if (vM[i + pos[0] - vis + 1][j + pos[1] - vis + 1] === 1 || !mask) {
-          pM[i][j] = [hM[i + pos[0] - vis + 1][j + pos[1] - vis + 1], bM[i + pos[0] - vis + 1][j + pos[1] - vis + 1]];
+          visHMap[i][j] = hM[i + pos[0] - vis + 1][j + pos[1] - vis + 1];
+          visBMap[i][j] = bM[i + pos[0] - vis + 1][j + pos[1] - vis + 1];
         } else {
-          pM[i][j] = [-1, -1];
+          visHMap[i][j] = -1;
+          visBMap[i][j] = -1;
         }
-      } else {
-        pM[i][j] = [-1, -1];
-      }
     }
   }
+  
+  var visPos = [vis - 1, vis - 1];
+  writeColorMap(generateColorMap(visBMap, visHMap), visPos);
+}
 
+function writeColorMap(map, pos) {
   NL();
   var e = document.createElement('canvas');
-  e.width = canvasScale * pM.length;
-  e.height = canvasScale * pM[0].length;
+  e.width = canvasScale * map.length;
+  e.height = canvasScale * map[0].length;
   var ctx = e.getContext("2d");
-  for (i = 0; i < pM.length; i++) {
-    for (j = 0; j < pM[i].length; j++) {
-      if (pM[i][j][1] === -1) {
-        ctx.fillStyle = "black";
-      } else if (pM[i][j][1] === 0) {
-        ctx.fillStyle = "blue";
-      } else if (pM[i][j][1] === 1) {
-        ctx.fillStyle = "hsl(100, 100%, " + (70 + pM[i][j][0] * 20) + "%)";
-      } else if (pM[i][j][1] === 2) {
-        ctx.fillStyle = "hsl(120, 100%, " + (20 + pM[i][j][0] * 10) + "%)";
-      } else if (pM[i][j][1] === 3) {
-        ctx.fillStyle = "hsl(60, 100%, " + (60 + pM[i][j][0] * 20) + "%)";
-      } else if (pM[i][j][1] === 4) {
-        ctx.fillStyle = "hsl(0, 0%, " + (50 - pM[i][j][0] * 20) + "%)";
-      } else if (pM[i][j][1] === 5) {
-        ctx.fillStyle = "hsl(40, 100%, " + (70 + pM[i][j][0] * 20) + "%)";
-      } else if (pM[i][j][1] === 6) {
-        ctx.fillStyle = "hsl(180, 60%, " + (50 + pM[i][j][0] * 20) + "%)";
-      }
+  var i, j;
+  for (i = 0; i < map.length; i++) {
+    for (j = 0; j < map[i].length; j++) {
+      ctx.fillStyle = map[i][j];
       ctx.fillRect(j * canvasScale, i * canvasScale, canvasScale, canvasScale);
-      if (i === vis - 1 && j === vis - 1) {
-        ctx.fillStyle = "#FF0000";
-        ctx.moveTo(j * canvasScale, i * canvasScale);
-        ctx.lineTo((j + 1) * canvasScale - 1, (i + 1) * canvasScale - 1);
-        ctx.stroke();
-        ctx.moveTo((j + 1) * canvasScale - 1, i * canvasScale);
-        ctx.lineTo(j * canvasScale, (i + 1) * canvasScale - 1);
-        ctx.stroke();
-      }
-    }
-  }
-  document.body.lastElementChild.appendChild(e);
-}
-
-function checkVisibility(pos, vis, hM, bM) {
-  var mapVis = JSON.parse(JSON.stringify(hM));
-  var mapGrad = JSON.parse(JSON.stringify(hM));
-  var posH;
-  if (bM[pos[0]][pos[1]] === 0) {
-    posH = -0.85;
-  } else {
-    posH = hM[pos[0]][pos[1]] + 0.15;
-  }
-  var i, j;
-  for (i = 0; i < mapVis.length; i++) {
-    for (j = 0; j < mapVis[i].length; j++) {
-      if (pos[0] - vis < i && pos[0] + vis > i && pos[1] - vis < j && pos[1] + vis > j) {
-        if (Math.pow(pos[0] - i, 2) + Math.pow(pos[1] - j, 2) <= Math.pow(vis, 2)) {
-          mapVis[i][j] = 1;
-        } else {
-          mapVis[i][j] = 0;
-        }
-      } else {
-        mapVis[i][j] = 0;
-      }
-    }
-  }
-  for (i = pos[0] - vis; i <= pos[0] + vis; i++) {
-    for (j = pos[1] - vis; j <= pos[1] + vis; j++) {
-      if (i >= 0 && j >= 0 && i < mapVis.length && j < mapVis[0].length) {
-        if (mapVis[i][j] === 1) {
-          if (bM[i][j] === 0) {
-            mapGrad[i][j] = (-1 - posH) / Math.sqrt(Math.pow(i - pos[0], 2) + Math.pow(j - pos[1], 2));
-          } else {
-            mapGrad[i][j] = (hM[i][j] - posH) / Math.sqrt(Math.pow(i - pos[0], 2) + Math.pow(j - pos[1], 2));
-          }
-        }
-      }
-    }
-  }
-  for (i = pos[0] - vis; i <= pos[0] + vis; i++) {
-    for (j = pos[1] - vis; j <= pos[1] + vis; j++) {
-      if (i >= 0 && j >= 0 && i < mapVis.length && j < mapVis[0].length) {
-        if (mapVis[i][j] === 1) {
-          var graddx = (j - pos[1]) / (i - pos[0]); // Essentially gradient from (i, j) to pos
-          var graddy = (i - pos[0]) / (j - pos[1]);
-          var a, b;
-          var visible = true;
-          if (i < pos[0]) {
-            for (a = 0; a < pos[0] - 1 - i; a++) {
-              var y = Math.floor((a + 0.5) * graddx + j + 0.5);
-              if (mapGrad[a + i][y] > mapGrad[i][j] || mapGrad[a + i + 1][y] > mapGrad[i][j]) {
-                visible = false;
-              }
-              /* if (i + 8 === pos[0] && j - 10 === pos[1]) {
-               *   console.log("i:" + i + "; j:" + j + "; graddx:" + graddx + "; graddy:" + graddy + "; posHeight:" + posH + "; posgradient:" + mapGrad[i][j] + "; x:" + (a + i) + "; y:" + y + "; height:" + hM[a + i][y] + "; gradient:" + mapGrad[a + i][y] + "; visible:" + visible);
-               *   console.log("i:" + i + "; j:" + j + "; graddx:" + graddx + "; graddy:" + graddy + "; posHeight:" + posH + "; posgradient:" + mapGrad[i][j] + "; x:" + (a + i + 1) + "; y:" + y + "; height:" + hM[a + i + 1][y] + "; gradient:" + mapGrad[a + i + 1][y] + "; visible:" + visible);
-               * } */
-            }
-          } else {
-            for (a = 0; a < i - 1 - pos[0]; a++) {
-              var y = Math.floor((a + 0.5) * graddx + pos[1] + 0.5);
-              if (mapGrad[a + pos[0]][y] > mapGrad[i][j] || mapGrad[a + pos[0] + 1][y] > mapGrad[i][j]) {
-                visible = false;
-              }
-            }
-          }
-          if (j < pos[1]) {
-            for (a = 0; a < pos[1] - 1 - j; a++) {
-              var x = Math.floor((a + 0.5) * graddy + i + 0.5);
-              if (mapGrad[x][a + j] > mapGrad[i][j] || mapGrad[x][a + j + 1] > mapGrad[i][j]) {
-                visible = false;
-              }
-            }
-          } else {
-            for (a = 0; a < j - 1 - pos[1]; a++) {
-              var x = Math.floor((a + 0.5) * graddy + pos[0] + 0.5);
-              if (mapGrad[x][a + pos[1]] > mapGrad[i][j] || mapGrad[x][a + pos[1] + 1] > mapGrad[i][j]) {
-                visible = false;
-              }
-            }
-          }
-          if (!visible) {
-            mapVis[i][j] = 0;
-          } else {
-            mapVis[i][j] = 1;
-          }
-        }
-      }
-    }
-  }
-  return mapVis;
-}
-
-function writeMapShaded(m) {
-  NL();
-  var i, j;
-  for (i = 0; i < m.length; i++) {
-    for (j = 0; j < m[0].length; j++) {
-      writeTxtSty("\u2588\u2588", "#" + Math.round((m[i][j] + 1.2) * 255 / 2.4).toString(16) + Math.round((m[i][j] + 1.2) * 255 / 2.4).toString(16) + Math.round((m[i][j] + 1.2) * 255 / 2.4).toString(16), true);
-    }
-    NL();
-  }
-  NL();
-}
-
-function writeMapBiome(m, hM, pos) {
-  var i, j;
-  NL();
-  var e = document.createElement('canvas');
-  e.width = canvasScale * m.length;
-  e.height = canvasScale * m[0].length;
-  var ctx = e.getContext("2d");
-  for (i = 0; i < m.length; i++) {
-    for (j = 0; j < m[i].length; j++) {
-      if (m[i][j] === -1) {
-        ctx.fillStyle = "black";
-      } else if (m[i][j] === 0) {
-        ctx.fillStyle = "blue";
-      } else if (m[i][j] === 1) {
-        ctx.fillStyle = "hsl(100, 100%, " + (70 + hM[i][j] * 20) + "%)";
-      } else if (m[i][j] === 2) {
-        ctx.fillStyle = "hsl(120, 100%, " + (20 + hM[i][j] * 10) + "%)";
-      } else if (m[i][j] === 3) {
-        ctx.fillStyle = "hsl(60, 100%, " + (60 + hM[i][j] * 20) + "%)";
-      } else if (m[i][j] === 4) {
-        ctx.fillStyle = "hsl(0, 0%, " + (50 - hM[i][j] * 20) + "%)";
-      } else if (m[i][j] === 5) {
-        ctx.fillStyle = "hsl(40, 100%, " + (70 + hM[i][j] * 20) + "%)";
-      } else if (m[i][j] === 6) {
-        ctx.fillStyle = "hsl(180, 60%, " + (50 + hM[i][j] * 20) + "%)";
-      }
-      ctx.fillRect(j * canvasScale, i * canvasScale, canvasScale, canvasScale);
-      if (i === pos[0] && j === pos[1]) {
+      if (pos !== null && i === pos[0] && j === pos[1]) {
         ctx.fillStyle = "#FF0000";
         ctx.moveTo(j * canvasScale, i * canvasScale);
         ctx.lineTo((j + 1) * canvasScale - 1, (i + 1) * canvasScale - 1);
@@ -355,151 +204,6 @@ function writeMapBiome(m, hM, pos) {
   }
   document.body.lastElementChild.appendChild(e);
   NL();
-}
-
-function initMap(mSize, mScale, mVariance, water) {
-  var size = [Math.ceil(mSize[0] / Math.pow(2, mScale)) * mScale + 1, Math.ceil(mSize[1] / Math.pow(2, mScale)) * mScale + 1];
-  var scale = Math.pow(2, mScale);
-  var rMap = new Array(size[0]);
-  var i, j, x, y;
-  for (i = 0; i < rMap.length; i++) {
-    rMap[i] = new Array(size[1]);
-  }
-  var variance = mVariance;
-  // Diamond square algorithm go!
-  for (i = 0; i < rMap.length; i += scale) {
-    for (j = 0; j < rMap[i].length; j += scale) {
-      if ((i <= scale || j <= scale || i >= rMap.length - scale || j >= rMap[i].length - scale) && water) {
-        x = i;
-        y = j;
-        while (x < 0) {
-          x += rMap.length;
-        }
-        while (y < 0) {
-          y += rMap[0].length;
-        }
-        rMap[x % rMap.length][y % rMap[0].length] = -1;
-      } else {
-        x = i;
-        y = j;
-        while (x < 0) {
-          x += rMap.length;
-        }
-        while (y < 0) {
-          y += rMap[0].length;
-        }
-        rMap[x % rMap.length][y % rMap[0].length] = Math.random() * 2 - 1;
-      }
-    }
-  }
-  while (scale > 1) {
-    // Diamond
-    var hS = scale / 2;
-    for (i = 0; i < rMap.length; i += scale) {
-      for (j = 0; j < rMap[i].length; j += scale) {
-        x = i + hS;
-        y = j + hS;
-        while (x < 0) {
-          x += rMap.length;
-        }
-        while (y < 0) {
-          y += rMap[0].length;
-        }
-        rMap[x % rMap.length][y % rMap[0].length] = ((getPoint(rMap, i, j) + getPoint(rMap, i, j + scale) + getPoint(rMap, i + scale, j) + getPoint(rMap, i + scale, j + scale)) / 4.0) + (Math.random() * 2 - 1) * variance;
-      }
-    }
-    // Square
-    for (i = 0; i < rMap.length; i += scale) {
-      for (j = 0; j < rMap[i].length; j += scale) {
-        x = i + hS;
-        y = j;
-        while (x < 0) {
-          x += rMap.length;
-        }
-        while (y < 0) {
-          y += rMap[0].length;
-        }
-        rMap[x % rMap.length][y % rMap[0].length] = ((getPoint(rMap, i, j) + getPoint(rMap, i + scale, j) + getPoint(rMap, i + hS, j - hS) + getPoint(rMap, i + hS, j + hS)) / 4.0) + (Math.random() * 2 - 1) * variance;
-        x = i;
-        y = j + hS;
-        while (x < 0) {
-          x += rMap.length;
-        }
-        while (y < 0) {
-          y += rMap[0].length;
-        }
-        rMap[x % rMap.length][y % rMap[0].length] = ((getPoint(rMap, i, j) + getPoint(rMap, i, j + scale) + getPoint(rMap, i - hS, j + hS) + getPoint(rMap, i + hS, j + hS)) / 4.0) + (Math.random() * 2 - 1) * variance;
-      }
-    }
-    variance /= 2;
-    scale /= 2;
-  }
-  return rMap;
-
-  // http://www.bluh.org/code-the-diamond-square-algorithm/
-}
-
-function initEmptyMap(mSize, mScale) {
-  var size = [Math.ceil(mSize[0] / Math.pow(2, mScale)) * mScale + 1, Math.ceil(mSize[1] / Math.pow(2, mScale)) * mScale + 1];
-  var map = new Array(size[0]);
-  for (var i = 0; i < size[0]; i++) {
-    map[i] = new Array(size[1]);
-    for (var j = 0; j < size[1]; j++) map[i][j] = -1;
-  }
-  return map;
-}
-
-function genMapBiome(mapHeight, mapHumidity) {
-  var mapBiome = JSON.parse(JSON.stringify(mapHeight));
-  var i, j;
-  for (i = 0; i < mapHeight.length; i++) {
-    for (j = 0; j < mapHeight[i].length; j++) {
-      if (mapHeight[i][j] < -0.8) {
-        mapBiome[i][j] = 0;
-      } else if (mapHumidity[i][j] < -0.8) {
-        mapBiome[i][j] = 3;
-      } else if (mapHeight[i][j] < -0.75) {
-        mapBiome[i][j] = 5;
-      } else if (mapHeight[i][j] < -0.5) {
-        if (mapHumidity[i][j] > 1.5) {
-          mapBiome[i][j] = 6;
-        } else {
-          mapBiome[i][j] = 1;
-        }
-      } else if (mapHeight[i][j] < 0.5) {
-        if (mapHumidity[i][j] < -0.75) {
-          mapBiome[i][j] = 3;
-        } else if (mapHumidity[i][j] > 0.5) {
-          mapBiome[i][j] = 2;
-        } else {
-          mapBiome[i][j] = 1;
-        }
-      } else if (mapHeight[i][j] < 0.75) {
-        if (mapHumidity[i][j] < -0.85) {
-          mapBiome[i][j] = 3;
-        } else if (mapHumidity[i][j] > 0.3) {
-          mapBiome[i][j] = 2;
-        } else {
-          mapBiome[i][j] = 1;
-        }
-      } else {
-        mapBiome[i][j] = 4;
-      }
-    }
-  }
-  return mapBiome;
-}
-
-function copyMaps(src, dest, vis) {
-  for (var i = 0; i < vis.length; i++) {
-    for (var j = 0; j < vis[i].length; j++) {
-      if (vis[i][j] === 1) {
-        dest[0][i][j] = src[0][i][j];
-        dest[1][i][j] = src[1][i][j];
-        dest[2][i][j] = src[2][i][j];
-      }
-    }
-  }
 }
 
 function getPoint(m, x, y) {
---
+++ mapTools.js
@@ -0,0 +1,271 @@
+/*
+ * 
+ * Terrain Map: Stores numerical values both positive and negative
+ * Biome Map: Stores integer biome ids (-1 for blank)
+ * Color Map: Stores color values in 'hsl(0, 0%, 0%)' form
+ * 
+ */
+
+var biomes = [
+    {"id":0, "name":"water",     "hue":"240", "height":[null, -0.8],  "humidity":[null, null]},
+    {"id":5, "name":"beach",     "hue":"40",  "height":[null, -0.75], "humidity":[null, null]},
+    {"id":4, "name":"rock",      "hue":"270", "height":[0.75, null],  "humidity":[null, null]},
+    {"id":3, "name":"desert",    "hue":"60",  "height":[null, null],  "humidity":[null, -0.75]},
+    {"id":6, "name":"marsh",     "hue":"180", "height":[null, null],  "humidity":[0.85, null]},
+    {"id":2, "name":"forest",    "hue":"120", "height":[null, null],  "humidity":[0.3, null]},
+    {"id":1, "name":"grassland", "hue":"100", "height":[null, null],  "humidity":[null, null]}
+];
+
+
+function initEmptyMap(mSize, mScale) {
+  var size = [Math.ceil(mSize[0] / Math.pow(2, mScale)) * mScale + 1, Math.ceil(mSize[1] / Math.pow(2, mScale)) * mScale + 1];
+  var map = new Array(size[0]);
+  for (var i = 0; i < size[0]; i++) {
+    map[i] = new Array(size[1]);
+    for (var j = 0; j < size[1]; j++) map[i][j] = -1;
+  }
+  return map;
+}
+
+function generateColorMap(biomeMap, heightMap) {
+  var colorMap = new Array(biomeMap.length);
+  var i, j, k;
+  for (i = 0; i < biomeMap.length; i++) {
+    colorMap[i] = new Array(biomeMap[i].length);
+    for (j = 0; j < biomeMap[i].length; j++) {
+      var biome = biomeMap[i][j];
+      if (biome === -1) {
+        colorMap[i][j] = "black";
+      } else if (biome === 0) {
+        colorMap[i][j] = "blue";
+      } else {
+        for(k = 0; k < biomes.length; k++) {
+          if(biome === biomes[k].id) colorMap[i][j] = "hsl(" + biomes[k].hue + ", 100%, " + (70 + heightMap[i][j] * 20) + "%)";
+        }
+      }
+    }
+  }
+  return colorMap;
+}
+
+function generateGradientMap(map) {
+  var colorMap = new Array(map.length);
+  var i, j;
+  for (i = 0; i < map.length; i++) {
+    colorMap[i] = new Array(map[i].length);
+    for (j = 0; j < map[i].length; j++) {
+      colorMap[i][j] = "hsl(0, 0%, " + (70 + map[i][j] * 20) + "%)";
+    }
+  }
+  return colorMap;
+}
+
+function checkMapVisibility(pos, vis, hM, bM) {
+  var mapVis = JSON.parse(JSON.stringify(hM));
+  var mapGrad = JSON.parse(JSON.stringify(hM));
+  var posH;
+  if (bM[pos[0]][pos[1]] === 0) {
+    posH = -0.85;
+  } else {
+    posH = hM[pos[0]][pos[1]] + 0.15;
+  }
+  var i, j;
+  for (i = 0; i < mapVis.length; i++) {
+    for (j = 0; j < mapVis[i].length; j++) {
+      if (pos[0] - vis < i && pos[0] + vis > i && pos[1] - vis < j && pos[1] + vis > j) {
+        if (Math.pow(pos[0] - i, 2) + Math.pow(pos[1] - j, 2) <= Math.pow(vis, 2)) {
+          mapVis[i][j] = 1;
+        } else {
+          mapVis[i][j] = 0;
+        }
+      } else {
+        mapVis[i][j] = 0;
+      }
+    }
+  }
+  for (i = pos[0] - vis; i <= pos[0] + vis; i++) {
+    for (j = pos[1] - vis; j <= pos[1] + vis; j++) {
+      if (i >= 0 && j >= 0 && i < mapVis.length && j < mapVis[0].length) {
+        if (mapVis[i][j] === 1) {
+          if (bM[i][j] === 0) {
+            mapGrad[i][j] = (-1 - posH) / Math.sqrt(Math.pow(i - pos[0], 2) + Math.pow(j - pos[1], 2));
+          } else {
+            mapGrad[i][j] = (hM[i][j] - posH) / Math.sqrt(Math.pow(i - pos[0], 2) + Math.pow(j - pos[1], 2));
+          }
+        }
+      }
+    }
+  }
+  for (i = pos[0] - vis; i <= pos[0] + vis; i++) {
+    for (j = pos[1] - vis; j <= pos[1] + vis; j++) {
+      if (i >= 0 && j >= 0 && i < mapVis.length && j < mapVis[0].length) {
+        if (mapVis[i][j] === 1) {
+          var graddx = (j - pos[1]) / (i - pos[0]); // Essentially gradient from (i, j) to pos
+          var graddy = (i - pos[0]) / (j - pos[1]);
+          var a, b;
+          var visible = true;
+          if (i < pos[0]) {
+            for (a = 0; a < pos[0] - 1 - i; a++) {
+              var y = Math.floor((a + 0.5) * graddx + j + 0.5);
+              if (mapGrad[a + i][y] > mapGrad[i][j] || mapGrad[a + i + 1][y] > mapGrad[i][j]) {
+                visible = false;
+              }
+              /* if (i + 8 === pos[0] && j - 10 === pos[1]) {
+               *   console.log("i:" + i + "; j:" + j + "; graddx:" + graddx + "; graddy:" + graddy + "; posHeight:" + posH + "; posgradient:" + mapGrad[i][j] + "; x:" + (a + i) + "; y:" + y + "; height:" + hM[a + i][y] + "; gradient:" + mapGrad[a + i][y] + "; visible:" + visible);
+               *   console.log("i:" + i + "; j:" + j + "; graddx:" + graddx + "; graddy:" + graddy + "; posHeight:" + posH + "; posgradient:" + mapGrad[i][j] + "; x:" + (a + i + 1) + "; y:" + y + "; height:" + hM[a + i + 1][y] + "; gradient:" + mapGrad[a + i + 1][y] + "; visible:" + visible);
+               * } */
+            }
+          } else {
+            for (a = 0; a < i - 1 - pos[0]; a++) {
+              var y = Math.floor((a + 0.5) * graddx + pos[1] + 0.5);
+              if (mapGrad[a + pos[0]][y] > mapGrad[i][j] || mapGrad[a + pos[0] + 1][y] > mapGrad[i][j]) {
+                visible = false;
+              }
+            }
+          }
+          if (j < pos[1]) {
+            for (a = 0; a < pos[1] - 1 - j; a++) {
+              var x = Math.floor((a + 0.5) * graddy + i + 0.5);
+              if (mapGrad[x][a + j] > mapGrad[i][j] || mapGrad[x][a + j + 1] > mapGrad[i][j]) {
+                visible = false;
+              }
+            }
+          } else {
+            for (a = 0; a < j - 1 - pos[1]; a++) {
+              var x = Math.floor((a + 0.5) * graddy + pos[0] + 0.5);
+              if (mapGrad[x][a + pos[1]] > mapGrad[i][j] || mapGrad[x][a + pos[1] + 1] > mapGrad[i][j]) {
+                visible = false;
+              }
+            }
+          }
+          if (!visible) {
+            mapVis[i][j] = 0;
+          } else {
+            mapVis[i][j] = 1;
+          }
+        }
+      }
+    }
+  }
+  return mapVis;
+}
+
+
+function initTerrainMap(mSize, mScale, mVariance, water) {
+  var size = [Math.ceil(mSize[0] / Math.pow(2, mScale)) * mScale + 1, Math.ceil(mSize[1] / Math.pow(2, mScale)) * mScale + 1];
+  var scale = Math.pow(2, mScale);
+  var rMap = new Array(size[0]);
+  var i, j, x, y;
+  for (i = 0; i < rMap.length; i++) {
+    rMap[i] = new Array(size[1]);
+  }
+  var variance = mVariance;
+  // Diamond square algorithm go!
+  for (i = 0; i < rMap.length; i += scale) {
+    for (j = 0; j < rMap[i].length; j += scale) {
+      if ((i <= scale || j <= scale || i >= rMap.length - scale || j >= rMap[i].length - scale) && water) {
+        x = i;
+        y = j;
+        while (x < 0) {
+          x += rMap.length;
+        }
+        while (y < 0) {
+          y += rMap[0].length;
+        }
+        rMap[x % rMap.length][y % rMap[0].length] = -1;
+      } else {
+        x = i;
+        y = j;
+        while (x < 0) {
+          x += rMap.length;
+        }
+        while (y < 0) {
+          y += rMap[0].length;
+        }
+        rMap[x % rMap.length][y % rMap[0].length] = Math.random() * 2 - 1;
+      }
+    }
+  }
+  while (scale > 1) {
+    // Diamond
+    var hS = scale / 2;
+    for (i = 0; i < rMap.length; i += scale) {
+      for (j = 0; j < rMap[i].length; j += scale) {
+        x = i + hS;
+        y = j + hS;
+        while (x < 0) {
+          x += rMap.length;
+        }
+        while (y < 0) {
+          y += rMap[0].length;
+        }
+        rMap[x % rMap.length][y % rMap[0].length] = ((getPoint(rMap, i, j) + getPoint(rMap, i, j + scale) + getPoint(rMap, i + scale, j) + getPoint(rMap, i + scale, j + scale)) / 4.0) + (Math.random() * 2 - 1) * variance;
+      }
+    }
+    // Square
+    for (i = 0; i < rMap.length; i += scale) {
+      for (j = 0; j < rMap[i].length; j += scale) {
+        x = i + hS;
+        y = j;
+        while (x < 0) {
+          x += rMap.length;
+        }
+        while (y < 0) {
+          y += rMap[0].length;
+        }
+        rMap[x % rMap.length][y % rMap[0].length] = ((getPoint(rMap, i, j) + getPoint(rMap, i + scale, j) + getPoint(rMap, i + hS, j - hS) + getPoint(rMap, i + hS, j + hS)) / 4.0) + (Math.random() * 2 - 1) * variance;
+        x = i;
+        y = j + hS;
+        while (x < 0) {
+          x += rMap.length;
+        }
+        while (y < 0) {
+          y += rMap[0].length;
+        }
+        rMap[x % rMap.length][y % rMap[0].length] = ((getPoint(rMap, i, j) + getPoint(rMap, i, j + scale) + getPoint(rMap, i - hS, j + hS) + getPoint(rMap, i + hS, j + hS)) / 4.0) + (Math.random() * 2 - 1) * variance;
+      }
+    }
+    variance /= 2;
+    scale /= 2;
+  }
+  return rMap;
+
+  // http://www.bluh.org/code-the-diamond-square-algorithm/
+}
+
+function generateBiomeMap(heightMap, humidityMap) {
+  var mapBiome = new Array(heightMap.length);
+  var i, j, k;
+  for (i = 0; i < heightMap.length; i++) {
+    mapBiome[i] = new Array(heightMap[i].length);
+    
+    for (j = 0; j < heightMap[i].length; j++) {
+      var he = heightMap[i][j];
+      var hu = humidityMap[i][j];
+      
+      for(k = 0; k < biomes.length; k++) {
+        var heB = biomes[k].height;   // Height bounds
+        var huB = biomes[k].humidity; // Humidity bounds
+        // Check bounds
+        if( (heB[0] === null || heB[0] < he) && (heB[1] === null || heB[1] > he) &&
+            (huB[0] === null || huB[0] < hu) && (huB[1] === null || huB[1] > hu)) {
+              mapBiome[i][j] = biomes[k].id;
+              k = biomes.length;
+        }
+      }
+    }
+  }
+  return mapBiome;
+}
+
+function copyMaps(src, dest, vis) {
+  for (var i = 0; i < vis.length; i++) {
+    for (var j = 0; j < vis[i].length; j++) {
+      if (vis[i][j] === 1) {
+        dest[0][i][j] = src[0][i][j];
+        dest[1][i][j] = src[1][i][j];
+        dest[2][i][j] = src[2][i][j];
+      }
+    }
+  }
+}
